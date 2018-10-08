bc19e2701a39206a9c98dab0e96f038e61dd9620
mr.soroush <soroush.shokuee@yahoo.com>
2018-10-08 15:11:50 +0330

    Practicing Data Binding...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home', //Selector as Tag
+  //selector: '.app-home', //Selector as Class
+  //selector: '[app-home]', //Selector as Property
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit{
+  
+  ngOnInit(){}
 
-  constructor() { }
+  constructor(){
+    setTimeout(() => {
+      this.allowClick = true;
+    }, 6000)
+  }
 
-  ngOnInit() {
+  age: number = 30;
+  name: string = 'Soroush';
+  allowClick = false;
+  btnClick = 'Button is not clicked.';
+  lastName = "";
+
+  getName(){
+    return this.name;
+  }
+
+  onButtonClicked(){
+    this.btnClick = 'Buttun is clicked now!';
+  }
+
+  onInputChange(event : Event){
+    this.lastName = (<HTMLInputElement>event.target).value;
+    //this.lastName = (event.target as HTMLInputElement).value;
   }
 
 }
