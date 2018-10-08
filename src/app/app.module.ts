d8ca3221106e741be1f72bfd6c274bdaac4699a7
mr.soroush <soroush.shokuee@yahoo.com>
2018-10-08 13:31:08 +0330

    first components...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import{ TestComponent } from './test.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'test-comp',
+    template: '<p>test...</p>',
+    styles: [
+        'p {color: #fb0000;}'
+    ]
+})
+
+export class TestComponent {
+
+}
